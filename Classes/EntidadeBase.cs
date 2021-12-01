00d0bcd1c1af1c16330f717a6bac7c0f76ce1a47
bia <bia@BIAZINHA>
2021-12-01 19:55:47 -0300

    Adicionar arquivos de projeto.

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace DIOSERIES
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }    
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,31 @@
+namespace DIOSERIES
+{
+    public enum Genero
+	{
+		Acao = 1,
+
+        Animacao = 2,
+
+		Aventura = 3,
+
+		Comedia = 4,
+
+		Documentario = 5,
+
+		Drama = 6,
+
+		Espionagem = 7,
+
+		Fantasia = 8,
+
+		Ficcao_Cientifica = 9,
+
+		Musical = 10,
+
+		Romance = 11,
+
+		Suspense = 12,
+
+		Terror = 13
+	}
+}
